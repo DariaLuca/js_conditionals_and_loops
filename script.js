c9de5d83ba78013ca7699f5ba7a8ba8974bb0ab9
DariaLuca <andreeadarialuca@gmail.com>
2024-03-11 20:15:18 +0200

    Add switch example

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ if(!isChoreDone || !hasEaten) {
         console.log('Inca nu ai terminat de mancat! Poti sa te uiti la desene cand farfuria este goala!');
     }
 }
+
+// if...else
+if(isChoreDone && hasEaten) {
+    console.log('Te poti uita la desene animate!');
+} else {
+    if(!isChoreDone) {
+        console.log()
+    }
+
+    if (!hasEaten) {
+        console.log('Inca nu ai terminat de mancat! Te poti uita la desene cand farfuria e goala!');
+    }
+}
+
+
+// if...else if...else
+
+let number = -13;
+
+if(number > 0) {
+    console.log('Numarul ales este pozitiv');
+} else if (number < 0) {
+    console.log('Numarul ales este negativ');
+} else {
+    console.log('Numarul ales este 0');
+}
+
+
+const dayOfTheWeek = 11;
+
+switch (dayOfTheWeek) {
+    case 1:
+        console.log('Ziua este Luni');
+        break;
+       case 2:
+        console.log('Nu stiu ce zi a saptamanii este aceasta');
+        break;
+      default:
+        console.log('Nu stiu ce zi a saptamanii este aceasta');
+        break;
+}
